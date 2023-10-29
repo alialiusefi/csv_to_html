41de8c1c5fd0808ec073ef87cb3b885fc215bb42
alialiusefi <alighanem99@live.com>
2023-10-29 21:38:33 +0900

    errors

--- src/keepassxc_csv_to_pdf/csv/model/parsing_errors.py
+++ src/keepassxc_csv_to_pdf/csv/model/parsing_errors.py
@@ -3,14 +3,21 @@ from dataclasses import dataclass
 
 @dataclass
 class ParsingError:
+    message: str
     pass
 
 
+@dataclass
 class EmptyValueError(ParsingError):
     field: str
-    message: str
     pass
 
     def __init__(self, field: str):
         self.field = field
         self.message = f"{self.field} cannot be empty!"
+
+
+@dataclass
+class IncorrectHeaderError(ParsingError):
+    def __init__(self):
+        self.message = "Incorrect header format."
