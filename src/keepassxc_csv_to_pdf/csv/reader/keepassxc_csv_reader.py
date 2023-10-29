7f6ce6c3a11dc2d3118a4c3be537141b0e02bb66
alialiusefi <alighanem99@live.com>
2023-10-29 21:38:18 +0900

    reader

---
+++ src/keepassxc_csv_to_pdf/csv/reader/keepassxc_csv_reader.py
@@ -0,0 +1,38 @@
+import csv
+from collections.abc import Iterator
+from typing import List
+
+from keepassxc_csv_to_pdf.csv.model.parsing_errors import ParsingError
+from keepassxc_csv_to_pdf.csv.model.keepassxc_csv_row import KeePassXCCSVRow
+
+
+class KeePassXCCSVReader:
+    file_path: str
+    __reader: Iterator
+    __ENCODING = "utf-8"
+
+    def __init__(self, filepath):
+        self.file_path = filepath
+        file = open(filepath, encoding="utf-8")
+        self.__reader = csv.reader(file)
+
+    def read_all(self) -> (List[ParsingError], List[KeePassXCCSVRow]):
+        list_of_rows = []
+        for row in self.__reader:
+            list_of_rows.append(row)
+        return list_of_rows
+
+    def read_row(self, row) -> (List[ParsingError], KeePassXCCSVRow):
+        obj = KeePassXCCSVRow(
+            group=row[0],
+            title=row[1],
+            username=row[2],
+            password=row[3],
+            url=row[4],
+            notes=row[5],
+            totp=row[6],
+            icon=row[7],
+            last_modified=row[8],
+            created=row[9],
+        )
+        return obj
