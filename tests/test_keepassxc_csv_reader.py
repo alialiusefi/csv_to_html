e9a8af6a6fd195cde1c0cda6415df6ad5b095165
alialiusefi <alighanem99@live.com>
2023-10-29 20:48:35 +0900

    fix test

--- tests/test_keepassxc_csv_reader.py
+++ tests/test_keepassxc_csv_reader.py
@@ -1,8 +1,8 @@
-from keepassxc_csv_to_pdf.csv.keepassxc_csv_reader import KeePassXCCSVReader
+from src.keepassxc_csv_to_pdf.csv.keepassxc_csv_reader import KeePassXCCSVReader
 
 
 def test_init():
-    file_path = "filepath.csv"
+    file_path = "fixtures/csv/test.csv"
     reader = KeePassXCCSVReader(file_path)
     assert reader.file_path == file_path
 
