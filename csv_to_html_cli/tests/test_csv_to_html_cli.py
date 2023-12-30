26b9edb37e75c5ed17b47cacd0b4ced7b58db5cd
alialiusefi <alighanem99@live.com>
2023-12-30 20:21:10 +0900

    improve tests

--- csv_to_html_cli/tests/test_csv_to_html_cli.py
+++ csv_to_html_cli/tests/test_csv_to_html_cli.py
@@ -1,8 +1,22 @@
 import os
 
-cli_command = "csvtohtml-cli"
+main = "../../csv_to_html_cli/src/csv_to_html_cli/__main__.py"
 
 
 def test_run_help():
-    exit_status = os.system('python3 ../../csv_to_html_cli/src/csv_to_html_cli/__main__.py -h')
+    command = f"python3 {main} -h"
+    exit_status = os.system(command)
+    assert exit_status == 0
+
+
+def test_convert_csv_to_html():
+    csv_file_path = "./fixtures/valid_csv.csv"
+    html_template_file = "./fixtures/template.html"
+    html_output_path = "/test_results/output.html"
+    command = (f"python3 {main} convert "
+               f"--csv_file {csv_file_path} "
+               f"--html_template_file {html_template_file} "
+               f"--html_output_path {html_output_path}")
+
+    exit_status = os.system(command)
     assert exit_status == 0
