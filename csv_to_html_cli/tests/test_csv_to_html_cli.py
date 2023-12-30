759da8d940444b3c6a4ea285d9d5dd461cd8dcef
alialiusefi <alighanem99@live.com>
2023-12-30 22:43:02 +0900

    fix test

--- csv_to_html_cli/tests/test_csv_to_html_cli.py
+++ csv_to_html_cli/tests/test_csv_to_html_cli.py
@@ -12,11 +12,11 @@ def test_run_help():
 def test_convert_csv_to_html():
     csv_file_path = "./fixtures/valid_csv.csv"
     html_template_file = "./fixtures/template.html"
-    html_output_path = "/test_results/output.html"
+    html_output_folder = "./test_results"
     command = (f"python3 {main} convert "
                f"--csv_file {csv_file_path} "
                f"--html_template_file {html_template_file} "
-               f"--html_output_path {html_output_path}")
+               f"--html_output_folder {html_output_folder}")
 
     exit_status = os.system(command)
     assert exit_status == 0
